e50c8cefb6332ddcecd809e98ad8fdd32765eef3
Vicram <vicramanjohfue@gmail.com>
2022-08-10 17:56:12 +0300

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
