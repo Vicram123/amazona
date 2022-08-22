c3a881ca200fd6a4f328b9139a211e562314a2c8
Vicram <vicramanjohfue@gmail.com>
2022-08-22 14:29:44 +0300

    Publish To Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import data from "./data.js";
+import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRoutes.js";
@@ -30,6 +30,12 @@ app.get("/api/keys/paypal", (req, res) => {
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use("/api/users", userRouter);
 app.use((err, req, res, next) => {
